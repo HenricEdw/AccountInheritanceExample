045e360a4f2ac8dca2f919d8445ccbdf67428072
Henric Edwards <62654315+HenricEdw@users.noreply.github.com>
2020-10-05 10:59:30 +0200

    Initial Commit
    First commit of inheritance of accounts

---
+++ SavingsAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance2
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; private set; }
+
+        public SavingsAccount(double interestRate, string accountOwner, string accountNumber)
+            : base(accountOwner, accountNumber)
+        {
+            InterestRate = interestRate;
+        }
+
+        public double CalculateInterestRate()
+        {
+            //Amount är protected, så vi kommer åt den här eftersom SavingsAccount ärver från Account
+            Amount = Amount * InterestRate;
+
+            return Amount;
+        }
+    }
+}
